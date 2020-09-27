0072caa10a31ddb31d89788e9e1e3075b535a07d
Nicksama88 <nicksama88@gmail.com>
2020-09-26 21:40:34 -0700

    initial commit with base files

---
+++ src/about.js
@@ -0,0 +1,13 @@
+const content = document.createElement("section");
+content.classList.add("content");
+const h1 = document.createElement("h1");
+const h2 = document.createElement('h2');
+h1.textContent = "Nick's Izakaya";
+h2.textContent = 'Serving authentic and traditional Japanese Cuisine, along with new contemporary favorites.  You know we are legitimate because we serve natto';
+
+const about = document.createElement("section");
+about.classList.add("about");
+const aH1 = document.createElement("h1");
+const aP = document.createElement("p");
+aH1.textContent = 'Irashaimasu!'
+aP.textContent = "We are a family run small business, with the goal of bringing the joy of Japanese cuisine to the lives of those in our community."
---
+++ src/index.js
@@ -0,0 +1,1 @@
+alert('things have run successfully!')
