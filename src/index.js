1318f55a6352e4b57da28c5bede708f460984409
Nicksama88 <nicksama88@gmail.com>
2020-09-27 17:24:37 -0700

    create module for rendering home content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import renderMain from './main'
 
 // Import CSS
 import './assets/CSS/style.css'
+import renderHome from './home';
 
+// build initial header and page
 const content = document.getElementById('content');
 content.appendChild(renderHeader());
 content.appendChild(renderMain())
+
+// insert home content to start
+const mainContent = document.getElementById('main-content');
+mainContent.appendChild(renderHome());
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 const renderMain = () => {
     const mainContent = document.createElement('div');
     mainContent.setAttribute('id', 'main-content');
-    mainContent.textContent = "main content will go here";
     return mainContent;
 }
     
