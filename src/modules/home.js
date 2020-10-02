4ed7aa7aefdb475678f079c020d7f9a8e210c424
Nicksama88 <nicksama88@gmail.com>
2020-10-01 20:41:17 -0700

    add image of natto to home page

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,14 +1,20 @@
 // render home content
+import natto from "../assets/images/natto.jpg"
 
 const renderHome = () => {
     let homeContainer = document.createElement('div');
     homeContainer.setAttribute("class", 'info-container');
     let p1 = document.createElement('p');
     let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let img = document.createElement('img');
     p1.textContent = 'Serving authentic and traditional Japanese cuisine, along with new contemporary favorites.';
     p2.textContent = "You know we are legitimate because we serve natto!";
-    homeContainer.append(p1, p2);
+    img.src = natto;
+    img.id = 'img-natto';
+    p3.appendChild(img);
+    homeContainer.append(p1, p2, p3);
     return homeContainer;
-}
+};
 
 export default renderHome
